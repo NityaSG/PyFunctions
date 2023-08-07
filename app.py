05495ddf56aebf4329c815de5616a1df9d48c256
Nitya Singh <76156577+NityaSG@users.noreply.github.com>
2023-08-07 16:56:27 +0530

    testing diabetes sample

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
-
+import pickle
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
 app = Flask(__name__)
 
 @app.route('/add')
@@ -8,6 +10,18 @@ def add():
     b = int(request.args.get('b'))
     result = a + b
     return f"The sum of {a} and {b} is {result}"
+
+@app.route('/predict')
+def predict():
+    with open('diabetes_model.pkl', 'rb') as file:
+        loaded_model = pickle.load(file)
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    new_input = [[a, b, 72, 35, 0, 33.6, 0.627, 50]]
+    prediction = loaded_model.predict(new_input)
+    result = a + b
+    return f"The sum of {a} and {b} is {prediction}"
+
 
 if __name__ == '__main__':
     app.run()
